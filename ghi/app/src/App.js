ee433b2222f83d4cd1733fc2b65ea040124859f9
adamazai0@gmail.com <adamazai0@gmail.com>
2023-03-06 15:53:28 -0800

    Manufacturer List

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
-
-function App() {
+import ManufacturerList from './ManufacturerList';
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path='manufacturers' element={<ManufacturerList manufacturers={props.manufacturers} />}  />
         </Routes>
       </div>
     </BrowserRouter>
--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+
+
+async function loadEverything(){
+  const manufacturerResponse = await fetch('http://localhost:8100/api/manufacturers/')
+
+  if (manufacturerResponse.ok){
+    const data = await manufacturerResponse.json()
+    root.render(
+      <React.StrictMode>
+        <App manufacturers={data.manufacturers} />
+      </React.StrictMode>
+    );
+  } else {
+    console.error(manufacturerResponse)
+  }
+}
+loadEverything()
