d652d6117d10eb6af61891fef1754d9e2690e79f
Joshua <jpe7619@gmail.com>
2023-03-07 15:49:59 -0800

    List Salespeople / Create Salesperson working on backend

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import customer_list
+from .views import customer_list, salesperson_list
 
 urlpatterns = [
-  path('customers/', customer_list, name="customer_list")
+  path('customers/', customer_list, name="customer_list"),
+  path('salespeople/', salesperson_list, name="salesperson_list"),
 ]
--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Customer
+from .models import Customer, Salesperson
 from django.http import JsonResponse
 from common.json import ModelEncoder
 import json
@@ -37,6 +37,38 @@ def customer_list(request):
     except:
       response = JsonResponse(
         {"message": "Invalid Customer Info"}
+      )
+      response.status_code = 400
+      return response
+
+class SalespersonEncoder(ModelEncoder):
+  model = Salesperson
+  properties = [
+    "name",
+    "employee_number",
+    "id",
+    ]
+
+@require_http_methods(["GET", "POST"])
+def salesperson_list(request):
+  if request.method == "GET":
+    salespeople = Salesperson.objects.all()
+    return JsonResponse(
+      {"salespeople": salespeople},
+      encoder=SalespersonEncoder,
+      safe=False
+    )
+  else:
+    try:
+      content = json.loads(request.body)
+      salespeople = Salesperson.objects.create(**content)
+      return JsonResponse(
+        {"salespeople": salespeople},
+        encoder=SalespersonEncoder,
+        safe=False)
+    except:
+      response = JsonResponse(
+        {"message": "Invalid Salesperson Info"}
       )
       response.status_code = 400
       return response
