f9de6a162a7fb0e8c360864c35b730b4f3c197d2
adamazai0@gmail.com <adamazai0@gmail.com>
2023-03-07 19:00:30 -0800

    Appointment View List as well as td of vip

--- ghi/app/src/AppointmentList.js
+++ ghi/app/src/AppointmentList.js
@@ -1,4 +1,33 @@
+import React, {useEffect, useState} from 'react'
+
 function ServiceAppointmentList(){
+    const [appointments, setAppointment]= useState([])
+
+
+
+    const fetchData = async () => {
+        const url = 'http://localhost:8080/api/appointments'
+        try{
+            const response = await fetch(url)
+            if(response.ok){
+                const data = await response.json()
+                setAppointment(data.appointments)
+            }
+        } catch (e){
+            console.error(e)
+        }
+    }
+    useEffect(() =>{
+        fetchData()
+    }, [])
+
+
+    const vip = async () => {
+        if (appointments.vip == true){
+            return (<img src="https://cdn-icons-png.flaticon.com/512/3557/3557655.png"/> )
+        }
+    }
+
     return (
         <>
         <table className="table table-striped">
@@ -13,13 +42,19 @@ function ServiceAppointmentList(){
           </tr>
         </thead>
         <tbody>
-            {/* {props.manufacturers.map((manufacturer) =>{
+            {appointments.map((appointment) =>{
                 return (
-                    <tr key={}>
-                        <td>{manufacturer.name}</td>
+                    <tr key={appointment.id}>
+                        <td>{appointment.owner_name}</td>
+                        {appointment.vip == true &&
+                            <td><img src='https://cdn-icons-png.flaticon.com/512/3557/3557655.png' width="40px"/></td>
+                        }
+                        {appointment.vip == false &&
+                            <td><img src="https://st.depositphotos.com/1054979/3064/v/450/depositphotos_30640773-stock-illustration-loser-stamp.jpg" width="60px"/></td>
+                        }
                     </tr>
                 )
-            })} */}
+            })}
         </tbody>
         </table>
         </>
