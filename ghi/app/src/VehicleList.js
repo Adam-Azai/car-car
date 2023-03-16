1004603b9479fbcc8096740f268ce72c79ccf185
adamazai0@gmail.com <adamazai0@gmail.com>
2023-03-16 11:59:10 -0700

    Model Cards

--- ghi/app/src/VehicleList.js
+++ ghi/app/src/VehicleList.js
@@ -1,9 +1,26 @@
 import React, { useEffect, useState } from 'react';
 
-
+function VehicleCard(props){
+    return(
+        <div className='col-sm'>
+            {props.list.map(vehicle => {
+                return (
+                    <div key ={vehicle.id} className="card text-center mb-0 mt-5 shadow">
+                        <img src={vehicle.picture_url} className="card-img-top" />
+                        <div className='card-body'>
+                            <h5 className="card-title">{vehicle.name}</h5>
+                            <p className="card-text"><small className="text-muted">{vehicle.manufacturer.name}</small></p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
 
 function VehicleList(){
-     const[vehicles, setVehicle] = useState([])
+     const[vehicles, setVehicle] = useState([],)
+     const [vehicleCard, setVehicleCards] = useState([],[],[],[])
 
      const fetchData = async () => {
         const url = 'http://localhost:8100/api/models/'
@@ -12,6 +29,41 @@ function VehicleList(){
             if(response.ok){
             const data = await response.json()
             setVehicle(data.models)
+
+
+
+            const requests = [];
+            for (let model of data.models) {
+              const detailUrl = `http://localhost:8100${model.href}`;
+              requests.push(fetch(detailUrl));
+            }
+            // Wait for all of the requests to finish
+            // simultaneously
+            const responses = await Promise.all(requests);
+            // Set up the "columns" to put the conference
+            // information into
+            const columns = [[], [], [], []];
+            // Loop over the conference detail responses and add
+            // each to to the proper "column" if the response is
+            // ok
+            let i = 0;
+            for (const modelResponse of responses) {
+              if (modelResponse.ok) {
+                const details = await modelResponse.json();
+                columns[i].push(details);
+                i = i + 1;
+                if (i > 3) {
+                  i = 0;
+                }
+              } else {
+                console.error(modelResponse);
+              }
+            }
+
+            // Set the state to the new list of three lists of
+            // conferences
+            setVehicleCards(columns);
+
             }
         } catch (e) {
             console.error(e)
@@ -24,7 +76,23 @@ function VehicleList(){
 
     return (
         <>
-        <table className="table table-striped">
+        <div className="custom-container" id='rat' >
+        <h2 className='text-center mt-3'>All Available Car Models</h2>
+        <div className="row ">
+            {vehicleCard.map((vehicleList, index) =>{
+                return (
+                    <VehicleCard key={index} list={vehicleList} />
+                )
+            })}
+        </div>
+        </div>
+        </>
+    )
+}
+export default VehicleList;
+
+
+         {/* <table className="table table-striped">
         <thead>
           <tr>
             <th>Vehicle models</th>
@@ -43,8 +111,4 @@ function VehicleList(){
                 )
             })}
         </tbody>
-        </table>
-        </>
-    )
-}
-export default VehicleList;
+        </table> */}
